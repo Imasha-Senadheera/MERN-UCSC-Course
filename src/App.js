7d6830d970cdefdf795a035febeb4e5b64547eb8
Imasha Senadheera <imashasenadheera326@gmail.com>
2024-07-14 15:02:15 +0530

    2nd Commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import "./App.css";
-import UserProfile from "./Components/userProfile"; // Import the UserProfile component
-import State from "./Components/count"; // Import the State component from count.jsx
-import Welcome from "./Components/hello"; // Import the Welcome component
+import SimpleCard from './Components/SimpleCard';
 
 function App() {
-  return (
-    <div>
-      <Welcome name="John Doe" />
-      <br></br>
-      <br></br>
-      <UserProfile name="John Doe" age={23} email="john.doe@example.com" />
-      <br></br>
-      <br></br>
-      <State />
-    </div>
-  );
-}
+  return <SimpleCard/>
+  }
+  
 
 export default App;
---
+++ src/Components/SimpleCard.js
@@ -0,0 +1,20 @@
+// src/components/SimpleCard.js
+import React from "react";
+import "./SimpleCard.css";
+
+function SimpleCard() {
+  return (
+    <div className="card-container">
+      <h1>Introduction</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat ipsum
+        eligendi nostrum quas sit. Veniam, molestiae ab repudiandae cum
+        exercitationem esse fugiat ratione perferendis ipsam quae ipsa eum
+        maxime quasi.
+      </p>
+      <button>View More</button>
+    </div>
+  );
+}
+
+export default SimpleCard;
