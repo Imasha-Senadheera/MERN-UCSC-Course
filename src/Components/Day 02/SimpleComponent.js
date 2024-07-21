4e02b014a53546b4e04b1837f5f95a5f2a52b480
Imasha Senadheera <imashasenadheera326@gmail.com>
2024-07-21 09:20:02 +0530

    6th Commit

--- src/Components/Day 02/SimpleComponent.js
+++ src/Components/Day 02/SimpleComponent.js
@@ -1,23 +1,88 @@
+// Codes for when Styles are in separate files 
+
+
+// import React from 'react';
+// import './SimpleComponent.css'; 
+
+// const SimpleComponent = () => {
+//   return (
+//     <div>
+//       <header>
+//         <h1>Simple React Component</h1>
+//       </header>
+
+//       <main>
+//         <p>Welcome to my simple React component!</p>
+//         <p>This is an example of a React component with external CSS.</p>
+//       </main>
+
+//       <footer>
+//         <p>&copy; {new Date().getFullYear()} Your Name</p>
+//       </footer>
+//     </div>
+//   );
+// };
+
+// export default SimpleComponent;
+
+
+
+// Codes for when Styles are in same files 
+
 import React from 'react';
-import './SimpleComponent.css'; 
+import styled from 'styled-components';
+
+const Body = styled.div`
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  background-color: #f0f0f0;
+`;
+
+const Header = styled.header`
+  background-color: #333;
+  color: #fff;
+  padding: 20px;
+  text-align: center;
+`;
+
+const Heading = styled.h1`
+  margin-top: 0;
+`;
+
+const Main = styled.main`
+  padding: 20rem;
+`;
+
+const Footer = styled.footer`
+  background-color: #333;
+  color: #fff;
+  text-align: center;
+  padding: 10px;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+`;
 
 const SimpleComponent = () => {
   return (
-    <div>
-      <header>
-        <h1>Simple React Component</h1>
-      </header>
+    <Body>
+      <Header>
+        <Heading>Simple React Component</Heading>
+      </Header>
 
-      <main>
+      <Main>
         <p>Welcome to my simple React component!</p>
         <p>This is an example of a React component with external CSS.</p>
-      </main>
+      </Main>
 
-      <footer>
+      <Footer>
         <p>&copy; {new Date().getFullYear()} Your Name</p>
-      </footer>
-    </div>
+      </Footer>
+    </Body>
   );
 };
 
 export default SimpleComponent;
+
+
