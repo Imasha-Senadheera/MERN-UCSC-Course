bbd0f54e7032847827bee0556811e8226171543b
Imasha Senadheera <imashasenadheera326@gmail.com>
2024-07-28 10:59:10 +0530

    26th Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-import UseMemo from "./Components/Day 03/Components/OtherHooks/UseMemo";
+import UseLayoutEffect from "./Components/Day 03/Components/OtherHooks/UseLayoutEffect";
 
 function App() {
   return (
     <div className="App">
-      <UseMemo />
+      <UseLayoutEffect />
     </div>
   );
 }
