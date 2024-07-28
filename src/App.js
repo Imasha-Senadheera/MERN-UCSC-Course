037a86bcfe35a5b8b59e73d73bf4fde65eb9ea60
Imasha Senadheera <imashasenadheera326@gmail.com>
2024-07-28 12:48:46 +0530

    33rd Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Controlled_Seperate from "./Components/Day 03/Components/ControlComponents/Controlled_Seperate";
+import Controlled_Object from "./Components/Day 03/Components/ControlComponents/Controlled_Object";
 
 function App() {
   return (
     <div className="App">
-      <Controlled_Seperate/>
+      <Controlled_Seperate />
+      <Controlled_Object />
     </div>
   );
 }
