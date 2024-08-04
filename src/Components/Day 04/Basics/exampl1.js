2411af1ab10a774ecd4a67fd289a9bb1f389be59
Imasha Senadheera <imashasenadheera326@gmail.com>
2024-08-04 09:18:45 +0530

    37 Commit

---
+++ src/Components/Day 04/Basics/exampl1.js
@@ -0,0 +1,15 @@
+//Variable store number data type
+let a = 35;
+console.log(typeof a);
+
+//Variable store string data type
+a = "GeeksForGeeks";
+console.log(typeof a);
+
+//Variable store number boolean type
+a = true;
+console.log(typeof a);
+
+//Variable store undifine (no value) data type
+a = undefined;
+console.log(typeof a);
