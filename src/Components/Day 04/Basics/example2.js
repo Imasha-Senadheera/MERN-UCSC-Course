a903496d6bf3f4e794251b63b5ed78c993c8c585
Imasha Senadheera <imashasenadheera326@gmail.com>
2024-08-04 09:23:57 +0530

    38 Commit

---
+++ src/Components/Day 04/Basics/example2.js
@@ -0,0 +1,11 @@
+//Using var
+var name = "Jhon";
+console.log(name); // Output: Jhon
+
+//Using let
+let age = 25;
+console.log(age); // Output: 25
+
+//Using const
+const PI = 3.221122;
+console.log(PI); // Output: 3.221122
