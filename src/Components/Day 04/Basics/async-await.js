9311d9148968c11526ed7db4606f2c1e90bdc401
Imasha Senadheera <imashasenadheera326@gmail.com>
2024-08-06 17:43:47 +0530

    46 Commit

---
+++ src/Components/Day 04/Basics/async-await.js
@@ -0,0 +1,16 @@
+const fs = require("fs").promises; // Node.js v10+
+
+// Async function to read a file
+async function readFileAsync(path) {
+  try {
+    console.log("Start reading file...");
+    const data = await fs.readFile(path, "utf8");
+    console.log(data);
+    console.log("File read successfully.");
+  } catch (err) {
+    console.error("An error occurred: ", err);
+  }
+}
+
+// Call the async function
+readFileAsync("input.txt");
